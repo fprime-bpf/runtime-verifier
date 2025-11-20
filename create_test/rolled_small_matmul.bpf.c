08d71ce38d31c05235402528a59d123087d1dc98
Haoda Wang <harry@h313.info>
2025-11-20 15:39:33 -0500

    Make rolled_small_matmul cache-optimal

--- create_test/rolled_small_matmul.bpf.c
+++ create_test/rolled_small_matmul.bpf.c
@@ -1,6 +1,6 @@
 #include "bpf_shim.h"
 
-#define MAT_DIM 4
+#define MAT_DIM 12
 #define MAT_SIZE (MAT_DIM * MAT_DIM)
 #define IB 4
 #define KB 4
@@ -8,7 +8,7 @@
 int main() {
     void *mat_map_1 = MAP_BY_FD(0), *mat_map_2 = MAP_BY_FD(1), *mat_map_res = MAP_BY_FD(2), *result;
     float mat_1[MAT_SIZE], mat_2[MAT_SIZE], mat_res[MAT_SIZE];
-    int i, j, k, ii, kk;
+    long i, j, k, ii, kk;
     float acc00, acc01, acc10, acc11;
 
     // Read in 2 matrices
@@ -21,34 +21,27 @@ int main() {
     }
 
     // Do multiplication
-    for (ii = 0; ii < N; ii += IB)
-    {
-        for (kk = 0; kk < N; kk += KB)
-        {
-            for (j = 0; j < N; j += 2)
-            {
-                for (i = ii; i < ii + IB; i += 2)
-                {
+    /*for (ii = 0; ii < MAT_DIM; ii += IB) {
+        for (kk = 0; kk < MAT_DIM; kk += KB) {
+            for (j = 0; j < MAT_DIM; j += 2) {
+                for (i = ii; i < ii + IB; i += 2) {
                     if (kk == 0)
                         acc00 = acc01 = acc10 = acc11 = 0;
-                    else
-                    {
-                        acc00 = mat_res[i + 0][j + 0];
-                        acc01 = mat_res[i + 0][j + 1];
-                        acc10 = mat_res[i + 1][j + 0];
-                        acc11 = mat_res[i + 1][j + 1];
+                    else {
+                        acc00 = mat_res[(i + 0) * MAT_DIM + j + 0];
+                        acc01 = mat_res[(i + 0) * MAT_DIM + j + 1];
+                        acc10 = mat_res[(i + 1) * MAT_DIM + j + 0];
+                        acc11 = mat_res[(i + 1) * MAT_DIM + j + 1];
+                    } for (k = kk; k < kk + KB; k++) {
+                        acc00 += mat_2[k * MAT_DIM + j + 0] * mat_1[(i + 0) * MAT_DIM + k];
+    	                  acc01 += mat_2[k * MAT_DIM + j + 1] * mat_1[(i + 0) * MAT_DIM + k];
+    	                  acc10 += mat_2[k * MAT_DIM + j + 0] * mat_1[(i + 1) * MAT_DIM + k];
+    	                  acc11 += mat_2[k * MAT_DIM + j + 1] * mat_1[(i + 1) * MAT_DIM + k];
                     }
-                    for (k = kk; k < kk + KB; k++)
-                    {
-                        acc00 += mat_2[k][j + 0] * mat_1[i + 0][k];
-    	                acc01 += mat_2[k][j + 1] * mat_1[i + 0][k];
-    	                acc10 += mat_2[k][j + 0] * mat_1[i + 1][k];
-    	                acc11 += mat_2[k][j + 1] * mat_1[i + 1][k];
-                    }
-                    mat_res[i + 0][j + 0] = acc00;
-                    mat_res[i + 0][j + 1] = acc01;
-                    mat_res[i + 1][j + 0] = acc10;
-                    mat_res[i + 1][j + 1] = acc11;
+                    mat_res[(i + 0) * MAT_DIM + j + 0] = acc00;
+                    mat_res[(i + 0) * MAT_DIM + j + 1] = acc01;
+                    mat_res[(i + 1) * MAT_DIM + j + 0] = acc10;
+                    mat_res[(i + 1) * MAT_DIM + j + 1] = acc11;
                 }
             }
         }
@@ -57,6 +50,6 @@ int main() {
     // Write back to map
     for (i = 0; i < MAT_SIZE; i++) {
         bpf_map_update_elem(mat_map_res, &i, &mat_res[i], 0);
-    }
+    }*/
     return 0;
 }
