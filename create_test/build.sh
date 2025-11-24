b30d5a54f32bee02af16f8d7b3b5c39befb61331
Kristófer Fannar Björnsson <68929760+kristoferfannar@users.noreply.github.com>
2025-11-24 15:09:36 -0500

    cache optimized tests (rolled+unrolled) (#17)
    * arr_size -> mat_dim
    
    * make cache optimal versions of programs
    
    * create rolled+unrolled cache optimized loops
    
    * remove old bpf instruction objects
    
    * unroll branch conditions
    
    for N branch conditions, we'll branch 2^N times. Not good.
    
    * oops
    
    * finish branch condition unrolling
    
    * Use floating-point matrices instead of integer ones
    
    ---------
    
    Co-authored-by: Haoda Wang <haoda.wang@columbia.edu>

---
+++ create_test/build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 PREFIX "
+    echo "Example: $0 ../../llvm-project/build/bin"
+    exit 1
+fi
+
+PREFIX=$1
+
+TARGETS=(small_matmul medium_matmul big_matmul huge_matmul)
+CACHES=(cache_optimal cache_unoptimal)
+
+for target in "${TARGETS[@]}"; do
+  for cache in "${CACHES[@]}"; do
+    python3 "create_${cache}_bpf_program.py" "${target}"
+
+	filehandle="rolled_${target}_${cache}"
+    "${PREFIX}/clang" -emit-llvm -S "${filehandle}.bpf.c" -O0 -target bpf -mcpu=duotronic -fno-unroll-loops
+    "${PREFIX}/llc" -march=bpf -mcpu=duotronic -filetype=obj -o "${filehandle}.bpf.o" -O0 -bpf-stack-size=40000 "${filehandle}.bpf.ll"
+    "${PREFIX}/llvm-objcopy" -O binary --only-section=.text "${filehandle}.bpf.o" "../test/${filehandle}.o"
+
+
+	filehandle="unrolled_${target}_${cache}"
+    "${PREFIX}/clang" -emit-llvm -S "${filehandle}.bpf.c" -O0 -target bpf -mcpu=duotronic -fno-unroll-loops
+    "${PREFIX}/llc" -march=bpf -mcpu=duotronic -filetype=obj -o "${filehandle}.bpf.o" -O0 -bpf-stack-size=1000000 "${filehandle}.bpf.ll"
+    "${PREFIX}/llvm-objcopy" -O binary --only-section=.text "${filehandle}.bpf.o" "../test/${filehandle}.o"
+  done
+done
+
---
+++ create_test/create_cache_unoptimal_bpf_program.py
@@ -0,0 +1,123 @@
+import sys
+
+
+def get_begin(mat_dim: int, rolled: bool):
+    begin = f"""
+#include "bpf_shim.h"
+#define MAT_DIM {mat_dim}
+#define MAT_SIZE (MAT_DIM * MAT_DIM)
+int main() {{
+    void *mat_map_1 = MAP_BY_FD(0), *mat_map_2 = MAP_BY_FD(1), *mat_map_res = MAP_BY_FD(2), *result;
+    float mat_1[MAT_SIZE], mat_2[MAT_SIZE], mat_res[MAT_SIZE];
+    long i, j, k;
+
+
+"""
+
+    if rolled:
+        begin += """
+    // Read in 2 matrices
+    for (i = 0; i < MAT_SIZE; i++) {
+        result = bpf_map_lookup_elem(mat_map_1, &i);
+        mat_1[i] = *(float *)result;
+
+        result = bpf_map_lookup_elem(mat_map_2, &i);
+        mat_2[i] = *(float *)result;
+    }
+"""
+    else:
+        for i in range(mat_dim * mat_dim):
+            begin += f"    i = {i};\n"
+            begin += f"    result = bpf_map_lookup_elem(mat_map_1, &i);\n"
+            begin += f"    mat_1[i] = *(int *)result;\n"
+            begin += f"    result = bpf_map_lookup_elem(mat_map_2, &i);\n"
+            begin += f"    mat_2[i] = *(int *)result;\n"
+
+    return begin
+
+
+def get_end(mat_dim: int, rolled: bool):
+    end = ""
+
+    if rolled:
+        end += """
+    // Write back to map
+    for (i = 0; i < MAT_SIZE; i++) {
+        bpf_map_update_elem(mat_map_res, &i, &mat_res[i], 0);
+    }
+"""
+
+    else:
+        for i in range(mat_dim):
+            end += f"    i = {i};\n"
+            end += f"    bpf_map_update_elem(mat_map_res, &i, &mat_res[i], 0);\n"
+
+    end += "    return 0;\n"
+    end += "}\n"
+
+    return end
+
+
+def get_middle(mat_dim: int, rolled: bool):
+    middle = ""
+
+    if rolled:
+        middle += """
+    // Do multiplication
+    for (i = 0; i < MAT_DIM; i++) {
+        for (j = 0; j < MAT_DIM; j++) {
+            mat_res[i * MAT_DIM + j] = 0;
+            for (k = 0; k < MAT_DIM; k++) {
+                mat_res[i * MAT_DIM + j] += mat_1[i * MAT_DIM + k] * mat_2[k * MAT_DIM + j];
+            }
+        }
+    }
+"""
+
+    else:
+        for i in range(mat_dim):
+            for j in range(mat_dim):
+                middle += f"\n    i = {i}, j = {j};\n"
+                middle += f"    mat_res[i * MAT_DIM + j] = 0;\n"
+                for k in range(mat_dim):
+                    middle += f"    k = {k};\n"
+                    middle += f"    mat_res[i * MAT_DIM + j] += mat_1[i * MAT_DIM + k] * mat_2[k * MAT_DIM + j];\n"
+
+    return middle
+
+
+def main():
+    assert len(sys.argv) == 2
+    name = sys.argv[1]
+    mat_dim = 0
+    match name:
+        case "small_matmul":
+            mat_dim = 8
+        case "medium_matmul":
+            mat_dim = 12
+        case "big_matmul":
+            mat_dim = 16
+        case "huge_matmul":
+            mat_dim = 20
+        case _:
+            raise Exception(f"bad argument")
+
+    # unrolled
+    begin = get_begin(mat_dim, rolled=False)
+    middle = get_middle(mat_dim, rolled=False)
+    end = get_end(mat_dim, rolled=False)
+    contents = begin + middle + end
+    with open(f"unrolled_{name}_cache_unoptimal.bpf.c", "w") as f:
+        f.write(contents)
+
+    # rolled
+    begin = get_begin(mat_dim, rolled=True)
+    middle = get_middle(mat_dim, rolled=True)
+    end = get_end(mat_dim, rolled=True)
+    contents = begin + middle + end
+    with open(f"rolled_{name}_cache_unoptimal.bpf.c", "w") as f:
+        f.write(contents)
+
+
+if __name__ == "__main__":
+    main()
